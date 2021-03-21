171e319564f461ee474fb8a200c527140c6a7f42
cheese-cake-cookie <my.cheesecake.cookie@gmail.com>
2021-03-21 15:29:24 +0900

    fix: PR review사항 반영
    - 불필요한 함수와 스타일 컴포넌트의 생성을 막기 위해 선언 영역 변경
    - 생성된 함수를 재사용 하기 위하여 useCallback hook을 사용하도록 변경함
    
    #3

--- src/components/Modal/ModalCollection.jsx
+++ src/components/Modal/ModalCollection.jsx
@@ -1,60 +1,60 @@
-import React, { useState } from 'react';
+import React, { useCallback, useState } from 'react';
 import styled from '@emotion/styled';
 import Modal from './index';
+
+/** style */
+const ModalCollectionSection = styled.section`
+  display: flex;
+  flex-direction: column;
+  margin: 20px;
+  padding: 15px;
+  background-color: #333;
+  color: #fff;
+`;
+
+const Title = styled.h1`
+  font-size: 30px;
+  font-family: 'Roboto', 'Helvetica', 'Arial', sans-serif;
+  font-weight: 400;
+  line-height: 1.235;
+  letter-spacing: 0.00735em;
+`;
+
+const ModalOptions = styled.section`
+  display: flex;
+  flex-direction: column;
+`;
+
+const ModalOptionLabel = styled.label`
+  margin: 5px 0px;
+`;
+
+const ShowUIButton = styled.button`
+  margin-top: 20px;
+  padding: 5px 15px;
+  border: 1px solid rgba(144, 202, 249, 0.5);
+  color: #90caf9;
+  background-color: transparent;
+`;
 
 export default function ModalCollection(props) {
   const [visible, setVisible] = useState(false);
   const [maskClosable, setMaskClosable] = useState(true);
 
-  const openModal = () => {
+  const openModal = useCallback(() => {
     setVisible(true);
-  };
+  }, []);
 
-  const closeModal = () => {
+  const closeModal = useCallback(() => {
     setVisible(false);
-  };
+  }, []);
 
-  const toggleMaskClosable = () => {
+  const toggleMaskClosable = useCallback(() => {
     setMaskClosable(!maskClosable);
-  };
-
-  /** style */
-  const ModalCollection = styled.section`
-    display: flex;
-    flex-direction: column;
-    margin: 20px;
-    padding: 15px;
-    background-color: #333;
-    color: #fff;
-  `;
-
-  const Title = styled.h1`
-    font-size: 30px;
-    font-family: 'Roboto', 'Helvetica', 'Arial', sans-serif;
-    font-weight: 400;
-    line-height: 1.235;
-    letter-spacing: 0.00735em;
-  `;
-
-  const ModalOptions = styled.section`
-    display: flex;
-    flex-direction: column;
-  `;
-
-  const ModalOptionLabel = styled.label`
-    margin: 5px 0px;
-  `;
-
-  const ShowUIButton = styled.button`
-    margin-top: 20px;
-    padding: 5px 15px;
-    border: 1px solid rgba(144, 202, 249, 0.5);
-    color: #90caf9;
-    background-color: transparent;
-  `;
+  }, [maskClosable]);
 
   return (
-    <ModalCollection>
+    <ModalCollectionSection>
       <Title>Modal</Title>
       <ModalOptions>
         <ModalOptionLabel htmlFor="maskClosable">
@@ -72,6 +72,6 @@ export default function ModalCollection(props) {
         <h1>MODAL TITLE</h1>
         <p>MODAL CONTENT</p>
       </Modal>
-    </ModalCollection>
+    </ModalCollectionSection>
   );
 }
