8e0cd383198aedd98f70e19caaaf978a2f7d0e2a
brightchul <abhbtbb1@gmail.com>
2021-03-23 19:27:52 +0900

    fix: apply COLOR to theme

--- src/asset/style/theme.jsx
+++ src/asset/style/theme.jsx
@@ -1,13 +1,31 @@
 import '@emotion/react';
 
+const PRIMARY_COLOR = COLOR.Blue;
+const SECONDARY_COLOR = COLOR.Red;
+
 const theme = {
   color: {
-    primary700: '#1565C0',
-    primary500: '#2196F3',
-    gray000: '#FFFFFF',
-    gray600: '#939FA5',
-    gray900: '#323D45',
-    warning: '#FFA000',
+    primary50: PRIMARY_COLOR[50],
+    primary100: PRIMARY_COLOR[100],
+    primary200: PRIMARY_COLOR[200],
+    primary300: PRIMARY_COLOR[300],
+    primary400: PRIMARY_COLOR[400],
+    primary500: PRIMARY_COLOR[500],
+    primary600: PRIMARY_COLOR[600],
+    primary700: PRIMARY_COLOR[700],
+    primary800: PRIMARY_COLOR[800],
+    primary900: PRIMARY_COLOR[900],
+
+    secondary50: SECONDARY_COLOR[50],
+    secondary100: SECONDARY_COLOR[100],
+    secondary200: SECONDARY_COLOR[200],
+    secondary300: SECONDARY_COLOR[300],
+    secondary400: SECONDARY_COLOR[400],
+    secondary500: SECONDARY_COLOR[500],
+    secondary600: SECONDARY_COLOR[600],
+    secondary700: SECONDARY_COLOR[700],
+    secondary800: SECONDARY_COLOR[800],
+    secondary900: SECONDARY_COLOR[900],
   },
 };
 
