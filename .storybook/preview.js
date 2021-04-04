99c79082ab8d31b7419cd9e7eaa099ebcdac1f95
brightchul <abhbtbb1@gmail.com>
2021-04-04 19:25:25 +0900

    chore: apply emotion theme setting to storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,10 +1,21 @@
+import { ThemeProvider } from '@storybook/theming';
+import { GlobalStyle, theme } from '../src/asset/style';
 
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
-}
+};
+
+export const decorators = [
+  (Story) => (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Story />
+    </ThemeProvider>
+  ),
+];
